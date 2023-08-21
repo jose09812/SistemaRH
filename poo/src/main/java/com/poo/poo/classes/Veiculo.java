529d3d393e90eea21d026b09104686878a4faab3
jose09812 <jose.f.santos22@aluno.senai.br>
2023-08-21 09:10:34 -0300

    Classes Veiculo e Conta

---
+++ poo/src/main/java/com/poo/poo/classes/Veiculo.java
@@ -0,0 +1,34 @@
+package com.poo.poo.classes;
+
+import org.slf4j.MarkerFactory;
+
+public class Veiculo {
+    
+    //atributos 
+    public String proprietario;
+    public String renavam;
+    public String marca ;
+    public String modelo;
+    public int ano;
+    public String cor;
+    public double quilometragem;
+
+    public void imprime(){
+        System.out.println(
+        this.renavam
+        );
+
+    }
+
+    //comportamentos
+    void calculaIPVA(){
+        //Metodo para calcular o ipva
+
+    }
+    void acelerar(){
+        // um veiculo acelera 
+
+
+    }
+
+}
